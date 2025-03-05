63a97bd837d3a81c14d2fd717163b728a1c60e8f
MPCode123 <mq.pham2003@gmail.com>
2025-03-05 15:45:51 -0500

    Create .project

--- src/main/assignment_3.py
+++ src/main/assignment_3.py
@@ -1,1 +1,30 @@
+def Euler(t0, y0, f, h, N):
+    
+    t = t0
+    y = y0
+    
+    for _ in range(N):
+        y = y + h * f(t,y)
+        t = t + h
+    
+    print(y)
+    
+    
+def RK(t0, y0, f, h, N):
+    
+    t = t0
+    y = y0
+    
+    for _ in range(N):
+        k1 = h * f(t,y)
+        k2 = h * f(t + h/2, y + k1/2)
+        k3 = h * f(t + h/2, y + k2/2)
+        k4 = h * f(t + h, y + k3)
+        
+        y = y + 1/6 * (k1 + 2*k2 + 2*k3 + k4)
+        t = t + h
+        
+    print(y)
 
+        
+    
--- src/test/test_assignment_3.py
+++ src/test/test_assignment_3.py
@@ -1,1 +1,19 @@
+import sys
+import os
+
+# For using functions from assignment_1 to test in test_assignment_1
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from src.main.assignment_3 import *
+
+def f(t,y):
+    return t - y**2
+
+# Euler Method
+Euler(0,1,f,0.2,10)
+
+
+
+
+# Runge-Kutta
+RK(0,1,f,0.2,10) 
 
