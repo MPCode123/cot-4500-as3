ab740439bff5f980e6a96c67c2106735e61c1abd
MPCode123 <mq.pham2003@gmail.com>
2025-03-05 16:44:43 -0500

    Comments

--- src/main/assignment_3.py
+++ src/main/assignment_3.py
@@ -3,28 +3,30 @@ def Euler(t0, y0, f, h, N):
     t = t0
     y = y0
     
+    # Euler Formula
     for _ in range(N):
-        y = y + h * f(t,y)
-        t = t + h
+        y = y + h * f(t,y) # standard calculation
+        t = t + h # increment to next value
     
-    print(y)
+    print(y) # Prints final value after N iterations
     
     
-def RK(t0, y0, f, h, N):
+def RK(t0, w0, f, h, N):
     
     t = t0
-    y = y0
+    y = w0
     
+    # Runge-Kutta order 4
     for _ in range(N):
-        k1 = h * f(t,y)
+        k1 = h * f(t,y) 
         k2 = h * f(t + h/2, y + k1/2)
         k3 = h * f(t + h/2, y + k2/2)
         k4 = h * f(t + h, y + k3)
         
-        y = y + 1/6 * (k1 + 2*k2 + 2*k3 + k4)
-        t = t + h
+        y = y + 1/6 * (k1 + 2*k2 + 2*k3 + k4) # this is w_i+1
+        t = t + h # increment to next value
         
-    print(y)
+    print(y) # Prints final value after N iterations
 
         
     
--- src/test/test_assignment_3.py
+++ src/test/test_assignment_3.py
@@ -5,15 +5,27 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from src.main.assignment_3 import *
 
+# Function
 def f(t,y):
     return t - y**2
 
 # Euler Method
-Euler(0,1,f,0.2,10)
+# Range: 0 < t < 2
+# Iterations: 10
+# Initial Point: f(0) = 1
+# h = (2 - 0) / 10 = 0.2
+
+Euler(0,1,f,0.2,10) # 1.2446380979332121
 
 
+print("\n")
 
 
 # Runge-Kutta
-RK(0,1,f,0.2,10) 
+# Range: 0 < t < 2
+# Iterations: 10
+# Initial Point: f(0) = 1
+# h = (2 - 0) / 10 = 0.2
+
+RK(0,1,f,0.2,10) # 1.251316587879806
 
